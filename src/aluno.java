5dc4f0fe3df66f37a8e8986728f7f1cc238a98f4
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-08-21 14:27:00 -0300

    aula de poo, fernando

---
+++ src/aluno.java
@@ -0,0 +1,22 @@
+public class aluno {
+    private int[] matricula;
+
+
+    public int[] getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int [] matricula) {
+        this.matricula = matricula;
+    }
+
+
+
+    public String toString() {
+        return "aluno{" +
+                "matricula=" + matricula +
+                '\'' +
+                '}';
+    }
+}
+
---
+++ src/vetor.java
@@ -0,0 +1,13 @@
+
+public class vetor {
+
+    private int [] vetor=new int[5];
+
+    public int[] getVetor() {
+        return vetor;
+    }
+
+    public void setVetor(int[] matricula) {
+        this.vetor = vetor;
+    }
+}
