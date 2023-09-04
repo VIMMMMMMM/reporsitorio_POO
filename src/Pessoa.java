42a2c1a203386cd9156cc9eba0f2441bcd3a0b05
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-09-03 21:12:09 -0300

    adicionando classes Pessoa e Professor.

---
+++ src/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa {
+    private String nome;
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
