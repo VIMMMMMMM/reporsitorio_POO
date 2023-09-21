93f796836788fb6d5823871285f801b7023a58f2
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-09-21 15:43:52 -0300

    modificando nome de classes

--- src/exercicio2/reserva.java
+++ src/exercicio2/reserva.java
@@ -1,17 +1,19 @@
 package exercicio2;
 
-import java.sql.Time;
+import exercicio2.DominioExecoes.DominioExecoes;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-public class exercicio1 {
+public class reserva {
         private Integer numeroDoQuarto;
         private Date checkIn;
         private Date checkOut;
         private static SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy");
-        public  exercicio1(Integer numeroDoQuarto, Date checkIn, Date checkOut){
-            super();
+        public  reserva(Integer numeroDoQuarto, Date checkIn, Date checkOut) throws DominioExecoes {
+            if (!checkOut.after(checkIn))
+                throw new DominioExecoes(" check out deve ser depois de check in") ;
             this.numeroDoQuarto= numeroDoQuarto;
             this.checkIn=checkIn;
             this.checkOut=checkOut;
@@ -36,13 +38,19 @@ public class exercicio1 {
         return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
 
     }
-    public void atualizacaoDeDatas(Date checkIn, Date checkOut){
+    public void  atualizacaoDeDatas(Date checkIn, Date checkOut) throws DominioExecoes {
+            Date agora = new Date();
+            if(checkIn.before(agora)||checkOut.before(agora))
+                throw new DominioExecoes(" datas de reserva devem ser futuras");
+            if (!checkOut.after(checkIn))
+                throw new DominioExecoes(" check out deve ser depois de check in") ;
             this.checkIn=checkIn;
             this.checkOut=checkOut;
+
     }
 
     @Override
     public String toString() {
-        return "quarto: "+numeroDoQuarto+", check in: "+dateFormat.format(checkIn)+", check out: "+dateFormat.format(checkOut)+", "+duracao()+"noites";
+        return "quarto: "+numeroDoQuarto+", check in: "+dateFormat.format(checkIn)+", check out: "+dateFormat.format(checkOut)+", "+duracao()+" noites";
     }
 }
