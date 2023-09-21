e99477cffa79d6869892c054060d47b40c3c9768
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-09-21 15:45:00 -0300

    corrigindo classe DominioExecoes

--- src/exercicio2/DominioExecoes/DominioExecoes.java
+++ src/exercicio2/DominioExecoes/DominioExecoes.java
@@ -1,7 +1,6 @@
 package exercicio2.DominioExecoes;
 
 public class DominioExecoes extends RuntimeException{
-    private static final long serialVersionUId=1L;
     public DominioExecoes(String msg){
         super(msg);
     }
